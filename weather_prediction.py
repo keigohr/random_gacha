90ef9571f216e16bbad3989dcb514dadeb5e738d
keigohr <83511899+keigohr@users.noreply.github.com>
2021-06-04 15:43:53 +0900

    Update weather_prediction.py

--- weather_prediction.py
+++ weather_prediction.py
@@ -46,9 +46,15 @@ def return_weather():
     weather_image=weather_image_list[2]
   elif '曇' in weather:
     weather_image=weather_image_list[1]
+    
+  #ツイートする文面を生成
+  tweet="今日の天気は{0}です。%0A最高気温:{1}度%0A最低気温:{2}度%0A降水確率:0時-6時は{3}%25,6時-12時は{4}%25,12時-18時は{5}%25,18時-24時は{6}%25".format(weather, temp_max, temp_min, precip_array[0], precip_array[1], precip_array[2], precip_array[3])
 
+  #背景画像の指定
+  background_image=r'background_img\weather.jpg'
+  
   #結果の出力
   weather_prediction=[weather, temp_max, temp_min, precip_array]
   test = [sg.Image(weather_image)]
   test.append([sg.Text(a) for a in weather_prediction])
-  return [test]
+  return tweet, background_image, [test]
