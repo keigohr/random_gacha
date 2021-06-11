d9499daa4c5743cdd97f413802541ee5d7a2f9c4
keigohr <83511899+keigohr@users.noreply.github.com>
2021-06-11 13:40:10 +0900

    Update and rename random_gacha.py to weather_random.py

---
+++ weather_random.py
@@ -0,0 +1,56 @@
+#明日の予想天気、最高気温、最低気温、降水確率をリストで出力する関数
+
+#ライブラリのインポート
+import requests
+from bs4 import BeautifulSoup
+import PySimpleGUI as sg
+import random
+
+def scrape_weather():
+  #tenki.jpの目的の地域のページのURL(東京都文京区)
+  url = 'https://tenki.jp/forecast/3/16/4410/13105/'
+
+  #HTTPリクエスト
+  r = requests.get(url)
+
+  bsObj = BeautifulSoup(r.content, "html.parser")
+
+  #10日間天気のまとまり
+  precip=bsObj.find(class_="forecast-point-week")
+
+  #10日間降水確率の取得
+  precip_strip=precip.find_all(class_="precip")
+  precip_array=[]
+  for item in precip_strip:
+    val=int(item.select_one(".precip .unit").previous_sibling)
+    precip_array.append(val)
+
+  return(precip_array)
+
+def generate_random():
+  x = random.random()
+  y = random.choice(scrape_weather())
+  lucky = x * y
+  return int(lucky)
+  
+def return_weather():
+  
+  #天気予報をランダムに生成する、天気予報に画像を対応させる
+  weather=None
+  weather_image=None
+  weather_list=['晴れ', '曇り', '雨', '暴風雨', '雷雨' ]
+  weather_image_list=[r'img\sunny_man.png', r'img\cloudy_image.png', r'img\rain_man.png', r'img\tenki_boufuu.png', r'img\thunder_girl.png']
+  index=generate_random() % 5
+  weather=weather_list[index]
+  weather_image=weather_image_list[index]
+    
+  #ツイートする文面を生成
+  tweet="今日の天気は{0}です。".format(weather)
+
+  #背景画像の指定
+  background_image=r'background_img\weather.jpg'
+  
+  #結果の出力
+  test = [sg.Image(weather_image)]
+  test.append([sg.Text(tweet)])
+  return tweet, background_image, [test]
